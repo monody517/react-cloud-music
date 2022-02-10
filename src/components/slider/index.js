e508e08c52a10b6858c242fccf72b4ec9f5899c0
wenpei <2737716714@qq.com>
2022-02-10 15:45:20 +0800

    轮播组件完成

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -3,14 +3,26 @@ import {SliderContainer} from "./style";
 import 'swiper/swiper-bundle.css'
 import Swiper, { Pagination, Autoplay } from 'swiper'
 
+Swiper.use([Pagination, Autoplay])
+
 
 function Slider(props) {
     const [sliderSwiper,setSliderSwiper] = React.useState(null)
     const {bannerList} = props
 
     React.useEffect(()=>{
-
-    })
+        if(bannerList.length && !sliderSwiper){
+            let newSliderSwiper = new Swiper(".slider-container",{
+                loop: true,
+                autoplay: {
+                    delay: 3000,
+                    disableOnInteraction: false,
+                },
+                pagination: {el:'.swiper-pagination'},
+            })
+            setSliderSwiper(newSliderSwiper)
+        }
+    },[bannerList.length,sliderSwiper])
 
     return (
         <SliderContainer>
@@ -18,9 +30,9 @@ function Slider(props) {
             <div className="slider-container">
                 <div className="swiper-wrapper">
                     {
-                        bannerList.map(slider=>{
+                        bannerList.map((slider,index)=>{
                             return (
-                                <div className="swiper-slide" key={slider.imgUrl}>
+                                <div className="swiper-slide" key={index}>
                                     <div className="slider-nav">
                                         <img src={slider.imgUrl} width="100%" height="100%" alt="推荐" />
                                     </div>
@@ -29,7 +41,7 @@ function Slider(props) {
                         })
                     }
                 </div>
-                <div className="swiper-pagination" />
+                <div className="swiper-pagination"></div>
             </div>
         </SliderContainer>
     )
